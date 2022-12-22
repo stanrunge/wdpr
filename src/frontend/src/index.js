e07fd7e9aa7a505b5789b08fc63aafd8bddae186
Stan Runge <stan@stanrunge.dev>
2022-12-23 00:10:31 +0100

    Add routing

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Error from './pages/Error/Error';
+import Informatie from './pages/Informatie/Informatie';
 
 const router = createBrowserRouter([{
     path: "/",
     element: <Landing />,
     errorElement: <Error />,
   },
+  {
+    path: "informatie",
+    element: <Informatie />,
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/frontend/src/pages/Error/Error.js
+++ src/frontend/src/pages/Error/Error.js
@@ -1,7 +1,12 @@
+import { useRouteError } from "react-router-dom";
+
 function Error() {
+  const error = useRouteError();
+  console.log(error);
+  
   return (
     <div>
-      <h1>404: Page not found</h1>
+      <h1>{error.status}: {error.statusText}</h1>
     </div>
   );
 }
